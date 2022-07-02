38410b4c03ec0731d053d8429fc6cb10428bad64
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-07-02 19:33:26 +0200

    some

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
-
 function App() {
-  return (
-    <div className="">
-     who is the first runner in world
-     
-    </div>
-  );
+  return <div className="">who is the first runner in world</div>;
 }
 
 export default App;
